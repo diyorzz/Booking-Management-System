c0642b0eb42991c11d626883fc2ea315c55dd74e
Diyor Akhmatov <128776299+diyorzz@users.noreply.github.com>
2024-11-15 10:48:57 +0500

    Add Docker-Compose

--- BookingManagement/BookingManagement/Program.cs
+++ BookingManagement/BookingManagement/Program.cs
@@ -1,5 +1,6 @@
 using BookingManagement.Components;
 using BookingManagement.Database;
+using BookingManagement.Extensions;
 using BookingManagement.Interfaces;
 using BookingManagement.Services;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +25,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    //app.ApplyMigrations();  
 }
 
 app.UseHttpsRedirection();
